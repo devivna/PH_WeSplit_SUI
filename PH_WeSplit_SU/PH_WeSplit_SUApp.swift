4b4c14af4ad397ea2c5fb6e29af6999a9cf8d854
Nadia <ivashchenko.nadia@gmail.com>
2023-02-17 18:38:05 +0200

    Initial Commit

---
+++ PH_WeSplit_SU/PH_WeSplit_SUApp.swift
@@ -0,0 +1,17 @@
+//
+//  PH_WeSplit_SUApp.swift
+//  PH_WeSplit_SU
+//
+//  Created by Students on 17.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct PH_WeSplit_SUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
